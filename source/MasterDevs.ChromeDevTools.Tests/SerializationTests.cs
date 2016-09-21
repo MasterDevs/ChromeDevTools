ba9d7ff63b320e621300123c3b5a1dc7aedb6361
Frederik Carlier <frederik.carlier@quamotion.mobi>
2016-09-22 00:39:45 +0200

    If the C# enum name does not match the JSON value, add an EnumMember attribute to ensure the mapping is done properly

---
+++ source/MasterDevs.ChromeDevTools.Tests/SerializationTests.cs
@@ -0,0 +1,21 @@
+﻿using MasterDevs.ChromeDevTools.Protocol.Chrome.DOM;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterDevs.ChromeDevTools.Tests
+{
+    [TestClass]
+    public class SerializationTests
+    {
+        [TestMethod]
+        public void ShadowRootTypeTest()
+        {
+            var value = JsonConvert.DeserializeObject<ShadowRootType>("\"user-agent\"");
+        }
+    }
+}
